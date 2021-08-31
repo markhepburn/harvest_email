b07962fb7bd29b9bd714989c0d067d20a08e4027
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-08-31 11:59:44 +1000

    Combine the two API calls to produce a list of my urgent projects

--- projects_status.py
+++ projects_status.py
@@ -35,16 +35,29 @@ class Config:
         return data
 
     def my_projects(self):
+        """Returns a list of projects the currently-authenticated user
+        is assigned to.  Useful for interactive testing."""
         assignments = self._api_get_list('https://api.harvestapp.com/api/v2/users/me/project_assignments',
                                       results_key='project_assignments')
         mine = [assignment for assignment in assignments if assignment['project']['is_billable']]
         return [assignment['project'] for assignment in mine]
 
     def active_projects(self):
+        """Returns a list of active projects.  Useful for interactive
+        testing."""
         active = self._api_get_list('https://api.harvestapp.com/v2/reports/project_budget',
                                       results_key='results',
                                       params={'is_active': 'true'})
         return active
+
+    def load_and_prioritise(self):
+        """Main data entry point; returns a list of projects in order
+        of most-to-least urgent"""
+        my_ids = set(project['id'] for project in self.my_projects())
+        details = [project for project in self.active_projects()
+                   if project['project_id'] in my_ids]
+        prioritised = sorted(details, key=lambda p: p['budget_remaining'] / p['budget'])
+        return prioritised
 
 
 if __name__ == '__main__':
