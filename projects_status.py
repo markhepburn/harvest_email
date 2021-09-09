ab352e74e5a105cbead3dc8b445869e76ec85495
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-09-09 20:13:40 +1000

    Remove unused configuration variable

--- projects_status.py
+++ projects_status.py
@@ -16,7 +16,6 @@ class HarvestAPI:
         # Throw a KeyError if configuration isn't set:
         self.account_id = envdict['HARVEST_ID']
         self.account_token = envdict['HARVEST_TOKEN']
-        self.account_user_id = envdict['HARVEST_USER_ID']
         self.account_email = envdict['ACCOUNT_EMAIL']
 
 
